5c67316cf7bce5f4f40276c9bb9484b8474e15bc
Eduardo Castro <37242504+skillerm@users.noreply.github.com>
2021-03-24 23:09:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,22 @@ var http = require('http').Server(app);
 const Remote = require("./modules/Master");
 const Gift = require("./modules/Gift");
 const dotenv = require('dotenv');
-const Master = new Remote("localhost", "postgres", "Hidden", "123456");
+const Master = new Remote("HOST", "USER", "DATABASE", "PASSWORDD");
 const pool = Master.Connection();
 
 dotenv.config();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+  extended: true
+}))
 
 
-require('./routes/Login')(app,Master,pool);
-require('./routes/Code')(app,Gift,pool);
+require('./routes/Login')(app, Master, pool);
+require('./routes/Code')(app, Gift, pool);
 //require('./routes/Devo')(app,Master,pool); ## TESTE
 
 
 http.listen(process.env.SERVER_PORT, function() {
-   console.log('CASTROMS')
-   console.log(`SERVER STARTED // PORT: ${process.env.SERVER_PORT}`);
+  console.log('CASTROMS')
+  console.log(`SERVER STARTED // PORT: ${process.env.SERVER_PORT}`);
 });
